c3aec87c20115184c51762e2222501ab091800f4
Jennifer Rondeau <jennifer.rondeau@konghq.com>
2021-08-19 13:02:31 -0400

    try to remove unwanted functionality

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -26,6 +26,7 @@ const sidebarNav = require("./site-config/sidebar-nav");
  * Install methods route builder
  */
 const releaseArray = require("./site-config/install-route-builder");
+const { off } = require("process");
 
 /**
  * Site Configuration
@@ -47,6 +48,9 @@ module.exports = {
     slackChannel: productData.slackChannelURL,
     docsDir: "docs",
     editLinks: false,
+    nextLinks: false,
+    prevLinks: false,
+    lastUpdated: false,
     sidebarDepth: 0,
     search: true,
     searchMaxSuggestions: 10,
